fcc5868344f67bec84ddc77e8e7a013be7150e76
Tanner <dolb.tanner@gmail.com>
2020-10-17 21:40:16 -0700

    Adds clear filter btn and updates styles

--- src/_includes/js/filter-posts.js
+++ src/_includes/js/filter-posts.js
@@ -1,11 +1,5 @@
-const allPosts = document.querySelectorAll(".post");
+const allPosts = [...document.querySelectorAll(".post")]; // convert iterable object (NodeList) to array
 console.log(allPosts); // Nodelist of article.post elements
-
-/* Get the post tag from the location interface using .search
-console.log(location.host);
-console.log(location.href);
-console.log(allPosts["0"].dataset.tags); // gets the tags ["html", "javascript"] from the dataset stringmap
-*/
 
 // search the location (broswer window) to search the URL for filter?=tag 
 // use slice to grab only the characters after the equal sign ie (=nunjucks)
@@ -13,17 +7,29 @@ const postTag = location.search.slice(location.search.indexOf("=") + 1);
 
 const postList = document.querySelector(".my-posts");
 const searchMsg = document.createElement("p");
-const clear = document.createElement("a");
+const clearBtn = document.createElement("a");
+const em = document.createElement("em");
+clearBtn.innerText = "clear filter";
+clearBtn.setAttribute("class", "clear-filter-btn");
+clearBtn.style.textDecoration = "none";
+clearBtn.style.paddingLeft = ".5rem";
+clearBtn.setAttribute("href", "/writing/");
 
 if (postTag) {
+
     // filter the posts nodelist for posts without the searched tag
-    let postsWithoutTag = Array.prototype.filter.call(allPosts, function(post) {
+    let postsWithoutTag = allPosts.filter(post => {
         return JSON.parse(post.dataset.tags).includes(postTag) === false;
     });
 
     // posts with tag (no use for now)
-    let postsWithTag = Array.prototype.filter.call(allPosts, function(post) {
+    let postsWithTag = allPosts.filter(post => {
         return JSON.parse(post.dataset.tags).includes(postTag);
+    });
+
+    // visually hide the elements not meeting tag query
+    postsWithoutTag.forEach(post => {
+        post.setAttribute("hidden", true);
     });
 
     if (postsWithTag.length === 1) {
@@ -34,9 +40,7 @@ if (postTag) {
 
     //searchMsg.innerText = `${postsWithTag.length} posts tagged with ${postTag}`;
     postList.prepend(searchMsg);
+    em.append(clearBtn);
+    searchMsg.append(em);
 
-    // visually hide the elements not meeting tag query
-    postsWithoutTag.forEach(post => {
-        post.setAttribute("hidden", true);
-    });
 }
