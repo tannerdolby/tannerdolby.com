5c9b792534e1bdffc507be4c1a0569dc854308cc
Tanner <dolb.tanner@gmail.com>
2020-11-11 01:02:36 -0800

    Adds back client-side js for ios touch events

--- src/_includes/js/ios-touch.js
+++ src/_includes/js/ios-touch.js
@@ -2,31 +2,23 @@
 const headingList = [...document.getElementsByClassName("post-heading")];
 const links = document.getElementsByClassName("direct-link");
 
-// console.log(headingList);
-
-
-// headingList.forEach(item => {
-//     console.log([...item.getElementsByTagName("a")]);
-
-// });
-
 // Iterate over the HTML collection and apply the direct link color change on touchstart event (iOS, safari, iphone)
 
-// function touchStart() {
-//     headingList.forEach(heading => {
-//         var links = heading.childNodes[1];
-//         links.style.color = "#444 !important";
-//     });
-// };
+function touchStart() {
+    headingList.forEach(heading => {
+        var links = heading.childNodes[1];
+        links.style.color = "#444 !important";
+    });
+};
 
-// function touchLeave() {
-//     headingList.forEach(heading => {
-//         var links = heading.childNodes[1];
-//         links.style.color = "transparent !important";
-//     });
-// }
+function touchLeave() {
+    headingList.forEach(heading => {
+        var links = heading.childNodes[1];
+        links.style.color = "transparent !important";
+    });
+}
 
-/*
+
 headingList.forEach(heading => {
     
     // Handling iOS/iPhone :hover for touch event 
@@ -36,4 +28,3 @@ headingList.forEach(heading => {
     heading.addEventListener("touchmove", touchLeave, false);
     heading.addEventListener("touchend", touchLeave, false);
 });
-*/
