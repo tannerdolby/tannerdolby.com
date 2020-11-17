92a9b9e22bad98ac2490f05477e446e086f10826
Tanner <dolb.tanner@gmail.com>
2020-11-17 11:12:14 -0800

    Refactoring some of the search script

--- src/_includes/js/search.js
+++ src/_includes/js/search.js
@@ -1,8 +1,8 @@
 /* 
 *   Creating Search Functionality for a static site
 * 
-*   1. Try out my idea with custom data attributes 
-*   2. Use Phil Hawksworths article from the eleventy tutorials section
+*   1. Try out my idea with custom data attributes (works!)
+*   2. Try Phil Hawksworths article from the eleventy tutorials section (haven't read yet)
 *
 */
 
@@ -13,31 +13,29 @@ const posts = [...document.getElementsByClassName("post")];
 
 // grab search bar to add keyup and click event handlers
 const searchBar = document.getElementById("search");
-const log = document.querySelector(".log");
 
 /**
  * Returns the text entered into an input search bar and injects it into an HTML element.
- * @param {Event} e The `input` event to capturecharacters entered into input field.
- * @retun {String} The text content injected into whatever element you choose.
+ * @param {Event} e The `input` event to capture user input to search bar.
+ * @retun {String} The text content user inputs to search.
  */
-function capture(e, element) {
-    return element.textContent = e.target.value;
+function getInput(e) {
+    return e.target.value;
 }
 
 searchBar.addEventListener("input", (e) => {
-    // inject user input to HTMLElement to add into search query
-    capture(e, log);
+    let userInput = getInput(e);
 
-    var searchQuery = [];
-    searchQuery.push(log.innerHTML.toLowerCase());
+    let searchQuery = [];
+    searchQuery.push(userInput.toLowerCase());
 
     // posts with title that matches each character in search query
-    const matchingPost = posts.filter((post) => {
+    const matchingPost = posts.filter(post => {
         return post.dataset.postTitle.toLowerCase().includes(searchQuery);
     });
 
     // posts with title that does not match the search query
-    const nonMatchingPost = posts.filter((post) => {
+    const nonMatchingPost = posts.filter(post => {
         return !post.dataset.postTitle.toLowerCase().includes(searchQuery);
     });
     
