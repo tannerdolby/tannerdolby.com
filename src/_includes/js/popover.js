99ada4093e8a31b25c3c039d7e084f9df1dac45a
Tanner <tannercdolby@gmail.com>
2021-11-17 01:22:58 -0800

    Deleting old files

--- src/_includes/js/popover.js
+++
@@ -1,9 +0,0 @@
-const closeBtn = document.querySelector(".pop-over .close");
-const popover = document.querySelector(".pop-over");
-
-function close() {
-    popover.setAttribute("aria-hidden", "true");
-    popover.classList.add("hide");
-}
-
-closeBtn.addEventListener("click", close);
